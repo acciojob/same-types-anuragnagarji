0bf35a46b2e39a93f040eaf34d6712288bd5bf27
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-20 22:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 //console.log(typeof value1);
 let value2 = prompt("Enter End Of the Range.");
-if(value1>=48&&value1<=57){
+if(value1[0]>=48&&value1[0]<=57){
 	value1=Number(value1);
 }
-if(value2>=48&&value2<=57){
+if(value2[0]>=48&&value2[0]<=57){
 	value2=Number(value2);
 }
 
