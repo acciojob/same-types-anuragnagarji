d6b5569bf017913845d861b7d54ceb855d3361f4
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-20 18:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function isSameType(value1, value2) {
-  if(typeof value1===typeof value2){
+  if( value1===typeof value2){
 	  return true;
   }
 	return false;
@@ -8,4 +8,5 @@ function isSameType(value1, value2) {
 // do not change the code below.
 //let value1 = prompt("Enter Start of the Range.");
 //let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(5, 5));
+alert(isSameType(typeof NaN,6);
+//alert(typeof NaN);
